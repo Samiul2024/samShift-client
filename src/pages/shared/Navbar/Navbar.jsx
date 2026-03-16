9e6395d44149b7c1e0902f40140cd2f5a2526384
Samiul2024 <mdsamiullahossen@gmail.com>
2026-03-17 00:09:05 +0600

    - Ensures better UX by visually indicating the current page.

--- src/pages/shared/Navbar/Navbar.jsx
+++ src/pages/shared/Navbar/Navbar.jsx
@@ -1,43 +1,97 @@
 import React from 'react';
-import { Link, NavLink } from 'react-router';
+import { Link, NavLink } from 'react-router'; // make sure to import from 'react-router-dom'
 import ProFastLogo from '../ProFastLogo/ProFastLogo';
 
 const Navbar = () => {
-    const navItems = <>
-        <li><NavLink to="/">Home</NavLink></li>
-        <li><NavLink to="/sendParcel">Send A Percel</NavLink></li>
-        <li><NavLink to="/coverage">Coverage</NavLink></li>
-        <li><NavLink to="/about">About Us</NavLink></li>
-        <li><NavLink to="/PricingCalculator">Pricing</NavLink></li>
-        <li><NavLink to="/TrackConsignment">Track Order</NavLink></li>
-        <li><NavLink to="/bearider">Be A Rider</NavLink></li>
+    // Function to apply active styles
+    const activeClass = ({ isActive }) =>
+        isActive ? 'rounded-3xl bg-[#CAEB66] font-bold' : 'text-gray-700';
 
-    </>
-    return (<div className="navbar bg-base-100 shadow-sm">
-        <div className="navbar-start">
-            <div className="dropdown">
-                <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
-                    <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"> <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /> </svg>
+    const navItems = (
+        <>
+            <li>
+                <NavLink to="/" className={activeClass}>
+                    Home
+                </NavLink>
+            </li>
+            <li>
+                <NavLink to="/sendParcel" className={activeClass}>
+                    Send A Parcel
+                </NavLink>
+            </li>
+            <li>
+                <NavLink to="/coverage" className={activeClass}>
+                    Coverage
+                </NavLink>
+            </li>
+            <li>
+                <NavLink to="/about" className={activeClass}>
+                    About Us
+                </NavLink>
+            </li>
+            <li>
+                <NavLink to="/PricingCalculator" className={activeClass}>
+                    Pricing
+                </NavLink>
+            </li>
+            <li>
+                <NavLink to="/TrackConsignment" className={activeClass}>
+                    Track Order
+                </NavLink>
+            </li>
+            <li>
+                <NavLink to="/bearider" className={activeClass}>
+                    Be A Rider
+                </NavLink>
+            </li>
+        </>
+    );
+
+    return (
+        <div className="navbar bg-base-100 shadow-sm">
+            <div className="navbar-start">
+                <div className="dropdown">
+                    <div
+                        tabIndex={0}
+                        role="button"
+                        className="btn btn-ghost lg:hidden"
+                    >
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            className="h-5 w-5"
+                            fill="none"
+                            viewBox="0 0 24 24"
+                            stroke="currentColor"
+                        >
+                            <path
+                                strokeLinecap="round"
+                                strokeLinejoin="round"
+                                strokeWidth="2"
+                                d="M4 6h16M4 12h8m-8 6h16"
+                            />
+                        </svg>
+                    </div>
+                    <ul
+                        tabIndex="-1"
+                        className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow"
+                    >
+                        {navItems}
+                    </ul>
                 </div>
-                <ul
-                    tabIndex="-1"
-                    className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
-                    {navItems}
-                </ul>
+
+                <ProFastLogo />
             </div>
-            {/* <a className="btn btn-ghost text-xl"> */}
-            <ProFastLogo></ProFastLogo>
-            {/* </a> */}
+
+            <div className="navbar-center hidden lg:flex">
+                <ul className="menu menu-horizontal px-1">{navItems}</ul>
+            </div>
+
+            <div className="navbar-end">
+                <Link className="btn btn-primary" to="/login">
+                    Login
+                </Link>
+            </div>
         </div>
-        <div className="navbar-center hidden lg:flex">
-            <ul className="menu menu-horizontal px-1">
-                {navItems}
-            </ul>
-        </div>
-        <div className="navbar-end">
-            <Link className='btn btn-primary' to="/login">Login</Link>
-        </div>
-    </div>
     );
 };
 
