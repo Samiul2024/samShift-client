5c280e58e7429a7e2a433f854589cdfe2bea4678
Samiul2024 <mdsamiullahossen@gmail.com>
2026-05-09 17:17:03 +0600

    chore: prepare frontend for production deployment

--- src/hooks/useAxios.jsx
+++ src/hooks/useAxios.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-    baseURL: `http://localhost:5000`
+    baseURL: `https://sam-shift-server.vercel.app`
 })
 const useAxios = () => {
     return axiosInstance;
--- src/hooks/useAxiosSecure.jsx
+++ src/hooks/useAxiosSecure.jsx
@@ -4,7 +4,7 @@ import useAuth from './useAuth';
 import { useNavigate } from 'react-router';
 
 const axiosSecure = axios.create({
-    baseURL: `http://localhost:5000`
+    baseURL: `https://sam-shift-server.vercel.app`
 })
 
 const useAxiosSecure = () => {
