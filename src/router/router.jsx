4e0975afdf5f419c6b1f21696688e4e19586177b
Samiul2024 <mdsamiullahossen@gmail.com>
2026-05-19 16:45:53 +0600

    debugging...login.

--- src/router/router.jsx
+++ src/router/router.jsx
@@ -4,7 +4,7 @@ import {
 import RootLayout from "../layouts/RootLayout";
 import Home from "../pages/Home/Home";
 import AuthLayout from "../layouts/AuthLayout";
-import Login from "../pages/Authentication/Login/Login";
+// import Login from "../pages/Authentication/Login/Login";
 import Register from "../pages/Authentication/Register/Register";
 import Coverage from "../pages/Coverage/Coverage";
 import PrivateRoute from "../routes/PrivateRoute";
@@ -93,10 +93,10 @@ export const router = createBrowserRouter([
         path: '/',
         Component: AuthLayout,
         children: [
-            {
-                path: 'login',
-                Component: Login
-            },
+            // {
+            //     path: 'login',
+            //     Component: Login
+            // },
             {
                 path: 'register',
                 Component: Register
