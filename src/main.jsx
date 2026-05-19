d4e641ef6a48b8bda83538588f126ed3fc864fb3
Samiul2024 <mdsamiullahossen@gmail.com>
2026-05-19 17:26:05 +0600

    fix vercel routing

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import {
-  RouterProvider,
-} from "react-router";
+// import {
+//   RouterProvider,
+// } from "react-router";
+import { RouterProvider } from "react-router-dom";
+
 import { router } from './router/router.jsx';
 import 'aos/dist/aos.css';
 import AOS from 'aos';
